32061c97b2bc947891cfb08059228647343b644f
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-07-12 17:40:01 +0400

    feat: Implemented Activity_1 Step 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/navbar';
 import Header from './components/header';
 import Footer from './components/footer';
+import { useState } from 'react';
 
 function App(): JSX.Element {
 
@@ -13,31 +14,7 @@ function App(): JSX.Element {
       <Header />
 
       <main>
-        <div className="cards__wrapper">
-          <div className="card">
-            <h3 className="card__text card__header">Cindy Clawford</h3>
-            <img
-              className="card__image"
-              src="someImageUrlHere"
-              alt="A cute tabby kitten"
-            ></img>
-            <p className="card__text">Species: Cat</p>
-            <p className="card__text">Favourite Food(s): Mice</p>
-            <p className="card__text">Birth Year: 2012</p>
-          </div>
-
-          <div className="card">
-            <h3 className="card__text card__header">Little Miss Purrfect</h3>
-            <img
-              className="card__image"
-              src="someImageUrlHere"
-              alt="A gray fluffy kitten"
-            ></img>
-            <p className="card__text">Species: Cat</p>
-            <p className="card__text">Favourite Food(s): Wet food, dry food</p>
-            <p className="card__text">Birth Year: 2016</p>
-          </div>
-        </div>
+        <div className="cards__wrapper">{/* JSX code can go here */}</div>
       </main>
 
       <Footer />
