28f0212bc4343b227cfb1747537ed173496ca9f0
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-07-12 18:17:40 +0400

    feat: Implemented activity_2 Step 1

---
+++ src/components/cat_card.tsx
@@ -0,0 +1,10 @@
+const catCard: React.FC = () => (
+  <div className="card">
+    <h3 className="card__text card__header">Mr Gwumpy</h3>
+    <p className="card__text">Species: cat</p>
+    <p className="card__text">Favourite Food(s): Caviar</p>
+    <p className="card__text">Birth Year: 1984</p>
+  </div>
+);
+
+export default catCard;
